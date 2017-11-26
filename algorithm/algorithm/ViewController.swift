03f2e2d9e16e68f9517c95933580c64f41f3437f
redmin1129 <redmin1129@gmail.com>
2017-11-26 13:13:32 +0900

    피보나치수열

--- algorithm/algorithm/ViewController.swift
+++ algorithm/algorithm/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("\(self.jumpCase(4))")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    //https://programmers.co.kr/learn/challenge_codes/153 멀리뛰기
+    
+    func jumpCase(_ n:Int) -> Int {
+        
+        if n == 0 {
+            return 0
+        } else if n == 1 {
+            return 1
+        } else if n == 2 {
+            return 2
+        }
+
+        return self.jumpCase(n-1) + self.jumpCase(n-2)
+    }
 
 }
 
